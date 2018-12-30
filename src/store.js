8affae58390f0bfc6ca58fb330b15c5cf3a256d4
nutmmm <nutmmm@organicdesign.co.nz>
2018-12-31 12:01:49 +1300

    Set up store state.

--- src/store.js
+++ src/store.js
@@ -5,7 +5,25 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
+		products: [
+			{
+				name: "Classic Tee",
+				price: 75,
+				image: "./assets/classic-tee.jpg",
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+				options: [
+					{
+						name: "SIZE",
+						required: true,
+						options: ["S", "M", "L"]
+					}
+				]
+			}
+		],
 
+		cart: [
+
+		]
 	},
 	mutations: {
 
